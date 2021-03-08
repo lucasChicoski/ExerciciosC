351d93e9922306d689f99336635025d77e5cfa57
Lucas <lucas.chicoski@outlook.com>
2021-03-08 18:25:17 -0300

    exercicio 2 feito

--- 02.c
+++ 02.c
@@ -2,8 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-void impirmir();
+void impirmir(int posixAnterior , int posixPosterior, int tamanhoVetor);
 
 int main(){
+
+    int posixAnterior , posixPosterior,  tamanhoVetor;
+
+    printf("Digite o priemiro numero da sequencia de fibonacci: \n");
+    scanf("%d", &posixAnterior);
+    printf("Digite o segundo numero da sequencia de fibonacci: \n");
+    scanf("%d", &posixPosterior);
+    printf("Digite o tamanho do vetor: \n");
+    scanf("%d", &tamanhoVetor);
+
+    printf("\n\n===============================================================\n\n");
+
+    impirmir(posixAnterior , posixPosterior , tamanhoVetor);
+
+    return 0;
+
+}
+
+void impirmir(int posixAnterior , int posixPosterior, int tamanhoVetor){
+    //implementando função fibonacci
+
+    int vetorFibonacci[tamanhoVetor];
+
+    vetorFibonacci[0] = posixAnterior;
+    vetorFibonacci[1] = posixPosterior;
+
+
+    for(int i = 2; i < tamanhoVetor; i++ ){
+        
+        vetorFibonacci[i] = vetorFibonacci[i-1] + vetorFibonacci[i-2];
+        printf("%d\n", vetorFibonacci[i]);
+    }
+
 
 }
